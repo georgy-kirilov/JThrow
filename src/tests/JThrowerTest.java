bd6b92752f5e80582ddc38fdf900f843fe25510b
georgy-kirilov <georgynk17@gmail.com>
2020-12-05 19:19:15 +0200

    Stable v1.0.0

--- src/tests/JThrowerTest.java
+++ src/tests/JThrowerTest.java
@@ -12,12 +12,5 @@ public class JThrowerTest
 				.isNullOrEmpty()
 				.has(em -> em.length() > 30)
 				.doesNotMatchPattern("^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$");
-		
-		
-		JThrower.throwIf(email.length(), "Email length")
-				.isLessThan(10);
-		
-		JThrower.throwIf("Georgi")
-				.has(x -> x.charAt(0) == 'G');
 	}
 }

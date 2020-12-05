717da241b42a93bf3aa15b9347c09350e8c9726b
georgy-kirilov <georgynk17@gmail.com>
2020-12-06 01:00:30 +0200

    Updated README

--- src/tests/Test.java
+++ src/tests/Test.java
@@ -1,11 +1,22 @@
 package tests;
 
+import java.util.Date;
+
 import core.JThrower;
 
 public class Test
 {
 	public static void main(String[] args)
 	{
+		Date minDate = new Date(1890, 1, 1);
+		Date maxDate = new Date();
+
+		Date userBirthdate = new Date(1888, 6, 6);
+
+		JThrower.throwIf(userBirthdate)
+		        .isNull()
+		        .isOutOfRange(minDate, maxDate);
+		
 		String email = "example@domain.com";
 		
 		JThrower.throwIf(email)
@@ -17,7 +28,8 @@ public class Test
 	    buyBeer(16);
 	}
 	
-	public static void buyBeer(int userAge) {
+	public static void buyBeer(int userAge) 
+	{
 	    JThrower.throwIf(userAge, "Age").isLessThan(18);
 	    System.out.println("\n\nBeer was sold");
 	}
